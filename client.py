fa6508333b713ca0139c9d739c5a339ab5940fb0
NIKITA-76 <89522179992.ru@gmail.com>
2023-09-17 00:28:55 +0300

    final

--- client.py
+++ client.py
@@ -3,6 +3,6 @@ import socket
 socket_srver = socket.socket()
 
 command = input('your command')
-socket_srver.connect(('192.168.0.102', 9090))
+socket_srver.connect(('10.137.84.60', 9090))
 
 socket_srver.send(command.encode())
--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import socket
+import subprocess
 
-new_socket = socket.socket()
-new_socket.bind(('localhost', 50))
+
+
+new_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+new_socket.bind(('192.168.0.102', 9090))
 new_socket.listen()
+
 
 conn, add = new_socket.accept()
 
+
 client = (conn.recv(1024).decode())
-print(client + 'Есть контакт')
+print(client)
+while True:
+    if client == '1':
+        subprocess.call(['cmd.exe', '/c', 'shutdown /h'])
+        print(' ' + client)
 
